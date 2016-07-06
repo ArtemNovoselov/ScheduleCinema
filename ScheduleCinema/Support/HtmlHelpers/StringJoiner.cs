27ea60cf07dd59cb523f4bac2867c45cb9976c8c
Artem Novoselov <devtim@pochta.ru>
2016-07-06 17:30:27 +0400

    Refactor index page

---
+++ ScheduleCinema/Support/HtmlHelpers/StringJoiner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using ScheduleCinema.Models;
+
+namespace ScheduleCinema.Support.HtmlHelpers
+{
+    public static class StringJoiner
+    {
+        public static MvcHtmlString Join(this HtmlHelper helper, IEnumerable<string> strings)
+        {
+            var str = string.Join(",", strings);
+            return new MvcHtmlString(str);
+        }
+    }
+}
